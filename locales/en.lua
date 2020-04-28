b60c5771438413883f9ee34ae3ffd854e133e2e0
Developer-Bear <44989435+Developer-Bear@users.noreply.github.com>
2020-04-27 19:54:36 -0500

    fixed improper use of locales while concatenating strings
    Other languages still need to be fixed.

--- locales/en.lua
+++ locales/en.lua
@@ -1,10 +1,10 @@
 Locales['en'] = {
-  ['novehnearby'] = 'No vehicle nearby',
+  ['novehnearby'] = 'There is vehicle nearby',
   ['cantinside']  = 'You cannot do this from inside a vehicle!',
-  ['arming']      = 'Arming the IED ',
-  ['vanilla']     = 'The IED will explode in %s seconds',
-  ['speed']       = 'The IED will explode at the speed of ',
-  ['remote']      = 'Make detonate the IED with ~INPUT_DETONATE~',
-  ['delayed']     = 'the timer will start as soon as someone gets in the car  (',
-  ['instant']     = 'The IED will explode as soon as the driver enters the car'  
+  ['arming']      = 'Arming the device',
+  ['vanilla']     = 'The device will explode in %s seconds',
+  ['speed']       = 'The device will explode at the speed of %s %s',
+  ['remote']      = 'Detonate the device by pressing G',
+  ['delayed']     = 'The device will detonate %s seconds after someone enters the vehicle',
+  ['instant']     = 'The device will explode as soon as the driver enters the car'  
 }
