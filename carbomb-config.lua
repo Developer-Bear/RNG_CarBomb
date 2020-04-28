b4dd834c05fdfadbd1e27d3103781aeeea686019
Developer-Bear <44989435+Developer-Bear@users.noreply.github.com>
2020-04-27 20:01:20 -0500

    Update carbomb-config.lua

--- carbomb-config.lua
+++ carbomb-config.lua
@@ -1,10 +1,28 @@
 Config = {}
 
+<<<<<<< HEAD
 Config.Locale = 'en' -- Choose Language
 
 -- 0 (Explode After Countdown) | 1 (Explode once the veh reaches a set speed) | 2 (Remote Detonate on Key Press) | 3 (Detonate after veh is entered and timer ends) |
 -- 4 (Detonate Immediately After the vehicle is entered)
 Config.DetonationType = 4
+=======
+-- Choose Language
+
+Config.Locale = 'it'
+
+-- Choose The Deflagration System
+
+Config.Vanilla = false
+
+Config.SpeedDeflagr = false
+
+Config.RemoteTrigger = true
+
+Config.DelayedTimer = false
+
+Config.InstantDeflagrSeatBusy = false
+>>>>>>> parent of b963b2c... Added info about usage
 
 Config.ProgressBarType = 1 -- 0 (Disable) | 1 (ProgressBars) | 2 (MythicProgressBars)
 
