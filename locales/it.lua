05fceb71bd25c208bb5d337b95b8da6be4feacdb
DarkSideOfTheCode <49038688+GianmarcoAquilano@users.noreply.github.com>
2020-08-27 23:08:57 +0200

    Update it.lua
    Update of Italian locales to suit a correct string format

--- locales/it.lua
+++ locales/it.lua
@@ -2,9 +2,9 @@ Locales['it'] = {
   ['novehnearby'] = 'Nessun veicolo nelle vicinanze',
   ['cantinside']  = 'Non puoi farlo dall\'interno del veicolo!', 
   ['arming']      = 'Piazzando la bomba...',
-  ['vanilla']      = 'La bomba esploderà tra ',
-  ['speed']      = 'La bomba esploderà alla velocità di ',
+  ['vanilla']     = 'La bomba esploderà tra %s secondi',
+  ['speed']       = 'La bomba esploderà alla velocità di %s %s',
   ['remote']      = 'Innesca il detonatore con G',
-  ['delayed']      = 'Il timer sarà avviato all\'entrata nell\'auto (',
-  ['instant']      = 'La bomba esploderà appena il guidatore entrerà in macchina'     
+  ['delayed']     = 'Il timer sarà avviato %s secondi dall\'entrata di una persona nel veicolo',
+  ['instant']     = 'La bomba esploderà appena il guidatore entrerà in macchina'     
 }
